8d78b99e7b532de1c254b4ed9add3d7c871afa17
Iuri Ferreira <76744994+devappbr@users.noreply.github.com>
2020-12-29 15:10:35 -0300

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'mysql',
+    connection: {
+      host : 'instance-selfood.clxblqyh8cpv.us-east-2.rds.amazonaws.com',
+      user : 'dba',
+      password : 'kdelnx99',
+      database : 'dbSelfood'
+    }
+  }
