09a1ef5cbc2afa71251eacc6f70922470c7f5525
Iuri Ferreira <76744994+devappbr@users.noreply.github.com>
2021-01-12 00:50:36 -0300

    alteração de endpoint do rds

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
     client: 'mysql',
     connection: {
-      host : 'mysqlserverdev.cwq9qv9m4vhn.sa-east-1.rds.amazonaws.com',
+      host : 'db-instance-czp-dev.cwq9qv9m4vhn.sa-east-1.rds.amazonaws.com',
       user : 'admin',
       password : 'Kdelnx99',
       database : 'dbcarzapio'
